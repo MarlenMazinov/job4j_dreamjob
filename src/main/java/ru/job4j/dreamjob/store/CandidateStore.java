5149926f8d823e0241c7025a191e85a0393ccc86
Marlen Mazinov <maraton.bashirov@yandex.ru>
2022-06-26 12:15:29 +0300

    5. Список Вакансий [#504842]

---
+++ src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
@@ -0,0 +1,40 @@
+package ru.job4j.dreamjob.store;
+
+import ru.job4j.dreamjob.model.Candidate;
+
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class CandidateStore {
+
+    private static final CandidateStore INST = new CandidateStore();
+
+    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+
+    private CandidateStore() {
+        candidates.put(1, new Candidate(1, "Junior Java Developer",
+                "Tech Stack : Java Core, Spring, Hibernate, Git, PostgreSQL."
+                        + "Experience 1 year",
+                new Date(2022, Calendar.MAY, 22, 17, 55)));
+        candidates.put(2, new Candidate(2, "Middle Java Developer",
+                "Stack: Java Core, Spring Framework, Hibernate, Kafka, MySQL, MongoDB"
+                        + "Experience 3 years",
+                new Date(2022, Calendar.JUNE, 11, 9, 22)));
+        candidates.put(3, new Candidate(3, "Senior Java Job",
+                "Java Core, Spring (Core, Boot, MVC, JPA, Security), Hibernate,"
+                        + " Oracle/PostgreSQL/Redis, REST, SOAP, Kafka/RabbitMQ, Docker,"
+                        + " Kubernetes/OpenShift, XML, JSON",
+                new Date(2022, Calendar.JUNE, 5, 13, 0)));
+    }
+
+    public static CandidateStore instOf() {
+        return INST;
+    }
+
+    public Collection<Candidate> findAll() {
+        return candidates.values();
+    }
+}
