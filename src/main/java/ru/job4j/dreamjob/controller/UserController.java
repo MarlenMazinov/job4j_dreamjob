fad8e7faa02fd011c960fbc6319a1223be967fd4
Marlen Mazinov <maraton.bashirov@yandex.ru>
2022-09-12 16:20:03 +0300

    4. Многопоточность в базе данных [#504860]

--- src/main/java/ru/job4j/dreamjob/controller/UserController.java
+++ src/main/java/ru/job4j/dreamjob/controller/UserController.java
@@ -1,19 +1,19 @@
 package ru.job4j.dreamjob.controller;
 
 import net.jcip.annotations.ThreadSafe;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.job4j.dreamjob.model.User;
 import ru.job4j.dreamjob.service.UserService;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
 
 @Controller
 @ThreadSafe
-public class UserController {
+public class UserController implements ErrorController {
     private final UserService userService;
 
     public UserController(UserService service) {
@@ -31,8 +31,18 @@ public class UserController {
         Optional<User> regUser = userService.add(user);
         if (regUser.isEmpty()) {
             model.addAttribute("message", "Пользователь с такой почтой уже существует");
-            return "redirect:/fail";
+            return "fail";
         }
         return "redirect:/success";
+    }
+
+    @GetMapping("/success")
+    String redirectToSuccess() {
+        return "success";
+    }
+
+    @GetMapping("/fail")
+    String redirectToFail() {
+        return "fail";
     }
 }
